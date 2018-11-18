0fac091f084836fcfd551c16317f5c683fa8a203
xopos <38457480+xopos@users.noreply.github.com>
2018-11-18 23:48:28 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car auto = new Car("Honda", "1999", 2, 120, 5);
+            Console.WriteLine(auto.GetCarName());
+            auto.AddPassenger();
+            auto.IncreaseSpeed(100);
+            Console.WriteLine(auto.currentspeed);
+            auto.IncreaseSpeed(200);
+            Console.WriteLine(auto.currentspeed);
+            auto.DecreaseSpeed(300);
+            auto.RemovePassenger();
+            Console.ReadKey();
+        }
+    }
+}
